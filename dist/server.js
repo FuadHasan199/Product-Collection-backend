62da8b34d107c53aff38f7e5a2a21ba9b033f9bc
MD Fuad <fuadhasa199@gmail.com>
2024-06-20 22:38:25 +0600

    eslint setup done

--- dist/server.js
+++ dist/server.js
@@ -13,14 +13,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-require("dotenv/config");
-const mongoose = require('mongoose');
+const config_1 = __importDefault(require("./config"));
+const mongoose = require("mongoose");
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            yield mongoose.connect(process.env.DB_URL);
-            app_1.default.listen(process.env.PORT, () => {
-                console.log(`Example app listening on port ${process.env.PORT}`);
+            yield mongoose.connect(config_1.default.db_url);
+            app_1.default.listen(config_1.default.port, () => {
+                console.log(`Example app listening on port ${config_1.default.port}`);
             });
         }
         catch (error) {
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,17 @@
 import app from "./app";
-import 'dotenv/config'
-const mongoose = require('mongoose');
+import config from "./config";
+
+const mongoose = require("mongoose");
 async function main() {
-    try{
-        await mongoose.connect(process.env.DB_URL);
-        
-        app.listen(process.env.PORT, () => {
-            console.log(`Example app listening on port ${process.env.PORT}`);
-          });
-    }catch(error){
-        console.log(error);
-    }
-    
+  try {
+    await mongoose.connect(config.db_url);
+
+    app.listen(config.port as string, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
   }
+}
 
- main()
-
-//   apollo
-//   MLuha7jf3b6TPwPm
+main();
